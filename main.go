def995a0bb96680eaf2b2d4aacaeae8c3be66a0e
Taiki-Arisue <arisue@supinf.co.jp>
2024-12-03 21:55:12 +0900

    change message

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello ddWorld!")
+		return c.String(http.StatusOK, "Hello World!")
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
